cab307ac1322707c8fc4a3dd12c329127be98100
Aadi Bhat <aadibhat09@gmail.com>
2024-10-20 15:02:16 -0700

    executable application

---
+++ Cleaner.ps1
@@ -0,0 +1,41 @@
+$initialSize = 0
+$clearedSize = 0
+
+$tempFolders = @("C:\Windows\Temp\*",
+                 "C:\Windows\SoftwareDistribution\Download\*",
+                 "C:\Windows\Minidump\*",
+                 "$home\AppData\Local\Temp\*",
+                 "$home\AppData\Local\Microsoft\Windows\*"
+                )
+
+# Get initial folder sizes
+foreach ($folder in $tempFolders) {
+    if (Test-Path $folder) {
+        $files = Get-ChildItem -Path $folder -Recurse -ErrorAction SilentlyContinue
+        $totalSize = ($files | Measure-Object -Property Length -Sum).Sum
+        $initialSize += $totalSize
+    }
+}
+
+# Clear temp files
+foreach ($folder in $tempFolders) {
+    if (Test-Path $folder) {
+        Remove-Item -Path $folder -Recurse -Force 2>$null
+    }
+}
+
+# Get the new folder sizes
+foreach ($folder in $tempFolders) {
+    if (Test-Path $folder) {
+        $files = Get-ChildItem -Path $folder -Recurse -ErrorAction SilentlyContinue
+        $totalSize = ($files | Measure-Object -Property Length -Sum).Sum
+        $clearedSize += $totalSize
+    }
+}
+
+# Calculate the space cleared in MB
+$spaceClearedMB = [math]::Round(($initialSize - $clearedSize) / 1MB, 2)
+
+# Output the result
+$custom = New-BTAppId -AppId "abcdefg"
+New-BurntToastNotification -Text "Temp files cleared", "$spaceClearedMB MB of temporary files have been cleared." -AppLogo "recycle.png"
--- clean.ps1
+++
@@ -1,9 +0,0 @@
-$PathToTempFolder = "$home\AppData\Local\Temp"
-$OriginalSize = (Get-ChildItem "$PathToTempFolder" -Recurse | Measure-Object -Property Length -Sum).Sum / 1MB
-
-Remove-Item -Path $PathToTempFolder -Recurse -Force 2>$null
-
-$NewSize = (Get-ChildItem "$PathToTempFolder" -Recurse | Measure-Object -Property Length -Sum).Sum / 1MB
-$SizeDifference = [int]($OriginalSize - $NewSize)
-
-New-BurntToastNotification -Text "Temp folder cleared", "$SizeDifference MB has been cleared from the temp folder." -AppLogo "C:\Users\aadib\Aadi\Scripts\CleanTempFolder\recycle.png"
